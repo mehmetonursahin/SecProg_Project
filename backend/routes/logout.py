3be537db053fdfb4c61d89564e7a5807b57f1a85
mehmetonursahin <sahinmehmetonur18@gmail.com>
2025-04-24 17:37:08 +0300

    session.clear() when logging out

--- backend/routes/logout.py
+++ backend/routes/logout.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from datetime import datetime
 
 logout_bp = Blueprint("logout", __name__, template_folder="../templates")
@@ -6,5 +6,6 @@ logout_bp = Blueprint("logout", __name__, template_folder="../templates")
 @logout_bp.route("/logout")
 def logout():
     # Handle logout (e.g., clear session, cookies)
+    session.clear()
     flash("You have logged out successfully.", "success")
     return redirect(url_for("login.login"))
